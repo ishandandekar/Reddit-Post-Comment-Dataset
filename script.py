258515c1788a9d523f32c8e8809444204520eb9b
Ishan Dandekar <ishandandekar2002@gmail.com>
2022-10-10 15:19:34 +0530

    Add modified code for better data production

--- script.py
+++ script.py
@@ -10,22 +10,49 @@ reddit_client = praw.Reddit(
     client_secret=os.getenv('CLIENT_SECRET'),
     user_agent=os.getenv('USER_AGENT')
 )
+DATA_DIR = os.path.join(os.getcwd(), 'data')
+print(f"Storing all the data in this directory: {DATA_DIR}")
 
 # Writing .txt files
 subreddits = ['confessions', 'tifu', 'talesfromtechsupport', 'IDontWorkHereLady',
-              'LetsNotMeet', 'AskReddit', 'todayilearned', 'explainlikeimfive', 'IAmA', 'FanTheories']
+              'LetsNotMeet', 'AskReddit', 'todayilearned', 'explainlikeimfive', 'IAmA', 'FanTheories', 'gonewildstories', 'UnethicalLifeProTips']
+
+# Setting up multiple loops for automation
+# for subreddit in subreddits:
+#     PATH = os.path.join(DATA_DIR, subreddit)
+#     os.mkdir(PATH)
+#     for submission in reddit_client.subreddit(subreddit).top(limit=15):
+#         post_id = 0
+#         comments_list = submission.comments.list()[:15]
+#         for comment in comments_list:
+#             data_id = 0
+#             with open(os.path.join(PATH, f'post_{post_id}.txt'), 'a') as f:
+#                 f.write(f'# {data_id}\n')
+#                 f.write(f'## r/{subreddit}\n')
+#                 f.write(f'### {submission.title}\n')
+#                 f.write(
+#                     f'#### {submission.selftext.encode("utf-8", errors="ignore")}\n')
+#                 f.write(f'##### {submission.url}\n')
+#                 f.write(
+#                     f'###### {comment.body.encode("utf-8", errors="ignore")}\n\n')
+#             data_id += 1
+#         post_id += 1
+#     break
 
 # Setting up multiple loops for automation
 for subreddit in subreddits:
-    for submission in reddit_client.subreddit(subreddit).top(limit=1):
-        post_count = 0
-        comments_list = submission.comments.list()[:15]
-        for comment in comments_list:
-            with open(f'data/{subreddit}.txt', 'a') as f:
-                f.write(f'# {post_count}\n')
-                f.write(f'## r/confessions\n')
-                f.write(f'### {submission.title}\n')
-                f.write(f'#### {submission.url}\n')
+    PATH = os.path.join(DATA_DIR, subreddit)
+    os.mkdir(PATH)
+    post_id = 0
+    for submission in reddit_client.subreddit(subreddit).top(limit=15):
+        with open(os.path.join(PATH, f'post_{post_id}.txt'), 'a') as f:
+            f.write(f'# r/{subreddit}\n')
+            f.write(f'## {submission.title}\n')
+            f.write(
+                f'#### {submission.selftext.encode("utf-8", errors="ignore")}\n\n')
+            comments_list = submission.comments.list()[:15]
+            for comment in comments_list:
                 f.write(
-                    f'##### {comment.body.encode("utf-8", errors="ignore")}\n\n')
-            post_count += 1
+                    f'###### {comment.body.encode("utf-8", errors="ignore")}\n')
+        post_id += 1
+    break
